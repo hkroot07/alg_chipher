cdfd846a3c24fd78e6177b3743a9faf82d8d6875
H4XOR_ <hakroot07@gmail.com>
2024-08-20 20:06:16 +0200

    Add function DeriveKeyFrom

--- processor/processor.go
+++ processor/processor.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	"golang.org/x/crypto/argon2"
 	"golang.org/x/term"
 )
+
+func DeriveKeyFrom(passphrase, salt []byte) ([]byte, error) {
+	key := argon2.IDKey(passphrase, salt, 1, 64*1024, 2, 32)
+	return key, nil
+}
 
 func GetPassphrase() ([]byte, error) {
 	println("Enter passphrase:")
