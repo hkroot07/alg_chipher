6959df34bd1bb296ff74a66d1e60df89abf7251f
H4XOR_ <hakroot07@gmail.com>
2024-08-20 18:49:35 +0200

    Create utils package

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "flag"
+import (
+	"errors"
+	"flag"
+)
 
 var cipherMode = flag.Bool("cipher", false, "Enable chipher mode.")
 var decipherMode = flag.Bool("decipher", false, "Enable decipher mode.")
 
 func main() {
+	flag.Parse()
 
+	if *cipherMode && *decipherMode {
+		utils.HaltOnErr(errors.New("please specify only one mode at  a time"))
+
+	}
+
+	if *decipherMode {
+
+	} else {
+
+	}
 }
