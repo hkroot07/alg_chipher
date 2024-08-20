b7b8193178b6c3eecfaa1d8ecc7b2f333924095b
H4XOR_ <hakroot07@gmail.com>
2024-08-20 18:44:19 +0200

    Add vars for flags

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "flag"
+
+var cipherMode = flag.Bool("cipher", false, "Enable chipher mode.")
+var decipherMode = flag.Bool("decipher", false, "Enable decipher mode.")
+
 func main() {
 
 }
